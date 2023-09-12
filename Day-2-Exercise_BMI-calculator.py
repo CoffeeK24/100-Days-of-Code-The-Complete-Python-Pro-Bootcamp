ac58efcd459cf13e310ac6a486f4f335726017a8
Kaan <144742564+CoffeeK24@users.noreply.github.com>
2023-09-12 16:07:09 +0300

    Update Day-2-Exercise_BMI-calculator.py

--- Day-2-Exercise_BMI-calculator.py
+++ Day-2-Exercise_BMI-calculator.py
@@ -1,10 +1,6 @@
-# 🚨 Don't change the code below 👇
 height = input("enter your height in m: ")
 weight = input("enter your weight in kg: ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
-height_n=float(height)
-weight_n=float(weight)
-bmi=weight_n/height_n**2
+height_n = float(height)
+weight_n = float(weight)
+bmi = weight_n / height_n**2
 print(int(bmi))
